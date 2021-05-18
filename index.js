5c6af6eaa89d97698367aa27a089c91f4a3b1693
devneser <zh_alex@yahoo.com>
2021-05-18 23:46:44 +0400

    refactor: update cors

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const firebase = require("./firebase/index");
 const port = process.env.PORT || 3000;
 const express = require("express");
 
-const socketServer = express()
+const app = express();
+
+//ENABLE CORS
+app.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+const socketServer = app
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(port, () => console.log(`Listening on ${port}`));
 
